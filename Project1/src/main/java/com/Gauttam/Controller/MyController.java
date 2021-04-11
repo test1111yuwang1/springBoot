a0bca2a38e6daa9252f3cb30bb4983aced242f1a
sarah888wang <33708681+sarah888wang@users.noreply.github.com>
2021-04-11 16:19:38 -0700

    Update MyController.java

--- Project1/src/main/java/com/Gauttam/Controller/MyController.java
+++ Project1/src/main/java/com/Gauttam/Controller/MyController.java
@@ -1,12 +1,6 @@
-test test test
-
 package com.Gauttam.Controller;
 
-
-
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
